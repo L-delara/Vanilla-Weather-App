bb292e904c54725a48a901481677b14222bbbffc
L-delara <92549675+L-delara@users.noreply.github.com>
2021-10-21 22:16:32 -0400

    Updated icon to refresh with city, fixed styling on buttons

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-function updateTime(date) {
+function updateTime(timestamp) {
+  let date = new Date(timestamp);
   let currentHour = date.getHours();
   let currentMinute = date.getMinutes();
   if (currentMinute < 10) {
     currentMinute = `0${currentMinute}`;
   }
 
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -15,33 +15,56 @@ function updateTime(date) {
     "Friday",
     "Saturday",
   ];
+  let dayIndex = date.getDay();
   let currentDay = days[dayIndex];
 
   return `${currentDay}, ${currentHour}:${currentMinute}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+
 function showTemp(response) {
-  document.querySelector("#current-city").innerHTML = response.data.name;
   document.querySelector("#today-temp").innerHTML = Math.round(
     response.data.main.temp
   );
-
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#today-description").innerHTML =
+    response.data.weather[0].description;
   document.querySelector("#humidity").innerHTML =
     response.data.main.humidity;
   document.querySelector("#windy").innerHTML = Math.round(
     response.data.wind.speed
   );
-
-  document.querySelector("#today-description").innerHTML =
-    response.data.weather[0].description;
+  document.querySelector("#weather-time-now").innerHTML = updateTime(
+    response.data.dt * 1000
+  );
   document.querySelector("#feels-like").innerHTML = Math.round(
     response.data.main.feels_like
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
   let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showTemp);
 }
 
@@ -51,17 +74,13 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-let changeTime = document.querySelector("#weather-time-now");
-let currentTime = new Date();
-changeTime.innerHTML = updateTime(currentTime);
-
 let searchForm = document.querySelector("#city-search");
 searchForm.addEventListener("submit", handleSubmit);
 
 function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let unit = "metric";
+  let unit = "imperial";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiKey = "4c9b53e4f8f5eb00df5915bdca340605";
   let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
